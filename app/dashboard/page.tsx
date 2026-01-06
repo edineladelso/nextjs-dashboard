15ec0d5dc3fc18c25d8c248338a57f44865e84b4
edineladelso <marengulae@gmail.com>
2026-01-06 21:09:58 +0200

    empurando o software

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import AcmeLogo from "@/app/ui/acme-logo";
+import { inter } from"@/app/ui/fonts";
+
+export default function Page() {
+  return <>
+    <main className={`${inter} antialiased flex min-h-screen flex-col `}>
+      Dashboard Page
+    </main>
+  </>
+}
